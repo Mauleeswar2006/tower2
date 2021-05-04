798d10d4adf99712c536cbaaff03fa30993e4660
Mauleeswar2006 <80914790+Mauleeswar2006@users.noreply.github.com>
2021-05-04 13:56:07 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,36 @@
+class Block{
+    constructor(x, y, width, height) {
+     // this.image = loadImage("block.png.png");
+        var options = {
+             restitution:1        
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("block.png");
+        this.Visiblity = 255;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        if(this.body.speed < 3){
+          push();
+          translate(pos.x, pos.y);
+          angleMode(RADIANS);
+          rotate(angle);
+          imageMode(CENTER);
+          image(this.image,0,0,this.width, this.height);
+          pop();
+        }
+         else{
+           World.remove(world, this.body);
+           push();
+           this.Visiblity = this.Visiblity - 5;
+           tint(255,this.Visiblity);
+           image(this.image, this.body.position.x, this.body.position.y, 30, 40);
+           pop();
+         }
+     
+      }
+}
